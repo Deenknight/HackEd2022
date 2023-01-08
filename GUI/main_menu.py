f6ac5af7cc7e5190e878acab79e12e4ebe3cc04d
oscar4203 <oscaritodeleonfiii@gmail.com>
2023-01-08 00:44:15 -0700

    ea

--- GUI/main_menu.py
+++ GUI/main_menu.py
@@ -10,7 +10,7 @@ root = ctk.CTk()
 
 #Test dictionary
 dict = {"Chainsawman": "Chainsaw URL", "Demon Slayer": "Demon URL", "Evangelion": "Eva URL", "Jujutsu Kaisen":
- "Jujutsu URL", "MHA": "MHA URL", "Naurto": "Naurto URL"}
+ "Jujutsu URL", "MHA (cringe)": "MHA URL", "Naurto": "Naurto URL"}
 
 dict2 = {"ChaCha": "Chainsaw URL", "DeDe": "Demon URL", "EvaEva": "Eva URL", "Juju":
  "Jujutsu URL", "MMMM": "MHA URL", "NARNAR": "Naurto URL"}
@@ -20,15 +20,19 @@ input = tk.StringVar(root)
 
 root.geometry("1000x600")#window size 
 root.title("E-Reader")#GUI title
+root.state('zoomed')
 
 
-def submit():
+
+def submit(list_box):
+    '''Extracts Manga Titles from the dictionary returned by the
+    extneral_search function'''
     print("button clicked!")
     name = input.get()
     print(f"name is: '{name}'")
     # external_search(webdriver.Firefox, name)
     #returns a dict of the external search results
-    if name == "pog":
+    if(name == "pog"):
         result_list = list(dict2.keys())
     else:
         result_list = list(dict.keys())
@@ -36,28 +40,19 @@ def submit():
     for i in range(len(result_list)):
         print(result_list[i])
     
-    search_results(result_list)
+    search_results(result_list, list_box, len(result_list))
     
 
-def search_results(items):
-    results = tk.Listbox(right_frame, width=150, height=150, font=("Comic Sans MS", 40))
-    results.pack(side='bottom', padx=100,pady=250)
+def search_results(items, list_box, num_items):
+    # results = tk.Listbox(right_frame, width=150, height=150, font=("Comic Sans MS", 40))
+    # results.pack(side='bottom', padx=100,pady=250)
+    if(num_items > 0):
+        list_box.delete(0, 'end')
+
     for i in items:
-        results.insert("end", i)
-   
-
-# img = Image.open(r"C:\Users\oscar\Documents\GitHubStuff\Ava.jpg")
-# img = img.resize((200,300), Image.ANTIALIAS)
-# my_img = ImageTk.PhotoImage(img)
-# my_label = tk.Label(image=my_img)
-# # my_label.image = my_img
-# my_label.pack()
-
+        list_box.insert("end", i)
 
 #left frame
-# bkg = ImageTk.PhotoImage(file= r"C:\Users\oscar\Documents\GitHubStuff\background.png")
-# left_frame = tk.Frame(root, width=350, height=500, bg='green')
-# left_frame.pack(side='left', expand=True)
 left_frame = ctk.CTkFrame(root, width=500, corner_radius=0)# corners become square when the corner_radius = 0 
 # left_frame['padding'] = 100
 left_frame.pack(side='left', padx=5, expand=True)
@@ -65,6 +60,11 @@ left_frame.pack(side='left', padx=5, expand=True)
 #right frame
 right_frame = ctk.CTkFrame(root, width=500, height=450)
 right_frame.pack(side='right', expand=True)
+
+
+#right frame widgets
+my_listbox = tk.Listbox(right_frame, width=150, height=150, font=("Comic Sans MS", 40))
+my_listbox.pack(side='bottom', padx=100,pady=50)
 
 #left frame widgets
 search_text = ctk.CTkLabel(left_frame, width=200, height=300, text="E-Reader", font=("Comic Sans MS", 30))
@@ -74,13 +74,10 @@ entry = ctk.CTkEntry(left_frame, textvariable=input, placeholder_text="Search Ma
 #NOTE: for some reason the placeholder text doesn't work when 'textvariable' is set as a parameter
 entry.pack(padx=20, pady=10)
 
-search_button = ctk.CTkButton(left_frame, command=submit, text="Submit", anchor= "bottom", font=("Comic Sans MS", 15))
+search_button = ctk.CTkButton(left_frame, command= lambda: submit(my_listbox), text="Submit", anchor= "bottom", font=("Comic Sans MS", 15))
 search_button.pack(pady=80)
 
-#right frame widgets
-# my_listbox = tk.Listbox(right_frame, width=150, height=150)
-# my_listbox.pack(side='bottom', padx=100,pady=50)
-
+# ...left_frame, command= lambda: submit(whatever u need to pass, items(?), button(?))
 
 
 
